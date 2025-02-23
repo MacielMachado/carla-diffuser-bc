463440eddaac068f1de70f4127c3ebad439d0aea
Gustavo Couto <gustavokarlcouto@gmail.com>
2025-02-23 11:29:43 -0300

    Extra steps from 8 to 0 in eval

--- eval_diffusion_bc_multimodality_birdview.py
+++ eval_diffusion_bc_multimodality_birdview.py
@@ -1007,7 +1007,7 @@ if __name__ == '__main__':
         guide_w=0.0,)
 
     # -----------------------------------------------------------------------------------------
-    extra_steps_list = [8]
+    extra_steps_list = [0]
     plotter = CarlaRoutePlotter(host='localhost', port=2030, town='Town01')
     for extra_steps in extra_steps_list:
         for model_path in models:
@@ -1021,7 +1021,7 @@ if __name__ == '__main__':
             eval_video_path = diff_bc_video_2 + model_path.split('/')[-1].split('.')[0] + f'_{0}' + '.mp4'
             if os.path.exists(eval_video_path[:-6]+"_map.png"):
                 continue
-            for i in range(50):
+            for i in range(25):
                 diff_bc_video = f'diff_bc_video_(diffuser)/birdview/new_arch_carla_route_plotter/{model_path.split("/")[1]}_{extra_steps}_extra_steps/'
                 diff_bc_video_2 = diff_bc_video + model_path.split('/')[-2] + '/'
                 os.makedirs(diff_bc_video_2, exist_ok=True)
