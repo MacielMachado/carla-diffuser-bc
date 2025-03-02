3646ee22f80d400ef275087a8d37072604902763
Gustavo Couto <gustavokarlcouto@gmail.com>
2025-03-02 09:07:34 -0300

    Run the other evaluation models.

--- eval_diffusion_bc_multimodality_birdview.py
+++ eval_diffusion_bc_multimodality_birdview.py
@@ -1045,8 +1045,8 @@ if __name__ == '__main__':
     plotter = CarlaRoutePlotter(host='localhost', port=2030, town='Town01')
     for extra_steps in extra_steps_list:
         for model_path in models:
-            if int(model_path.split('.')[0].split('_')[-1]) % 20 != 0:
-                continue
+            # if int(model_path.split('.')[0].split('_')[-1]) % 20 != 0:
+            #     continue
             model.load_state_dict(torch.load(model_path))
             persist_points = None
             diff_bc_video = f'diff_bc_video_(diffuser)/birdview/new_arch_carla_route_plotter/{model_path.split("/")[1]}_{extra_steps}_extra_steps/'
