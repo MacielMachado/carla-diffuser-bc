02d64ece0d655f1ea871e3bf3353aeef27cc413f
Gustavo Couto <gustavokarlcouto@gmail.com>
2025-03-05 15:29:31 -0300

    Change save folder name

--- learn_bc_multimodality_new_arch.py
+++ learn_bc_multimodality_new_arch.py
@@ -225,9 +225,9 @@ class TrainerNewArch():
         return model
 
     def save_model(self, model, ep=''):
-        os.makedirs(os.getcwd()+'/model_pytorch/Diffusion_BC_Multi_Multiple_New_Arch/'+self.name, exist_ok=True)
+        os.makedirs(os.getcwd()+'/model_pytorch/BC_Multi_Multiple_New_Arch/'+self.name, exist_ok=True)
         model_name = self.name+'_'+self.get_git_commit_hash()[0:4]+'_ep_'+f'{ep}'+'.pkl'
-        torch.save(model.state_dict(), os.getcwd()+'/model_pytorch/Diffusion_BC_Multi_Multiple_New_Arch/'+model_name)
+        torch.save(model.state_dict(), os.getcwd()+'/model_pytorch/BC_Multi_Multiple_New_Arch/'+model_name)
         return model_name
 
     def create_env(self):
