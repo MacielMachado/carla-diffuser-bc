b09bd5aefbcf3b9f23aa2a71b18eada6f47a52ac
Gustavo Couto <gustavokarlcouto@gmail.com>
2025-03-06 21:19:19 -0300

    muda o nome da screen do eval_diffusion_bc_multimodality_birdview_histogram.s

--- eval_diffusion_bc_multimodality_birdview_histogram.sh
+++ eval_diffusion_bc_multimodality_birdview_histogram.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/
 export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
-screen -L -S action_analyzer .venv/bin/python eval_diffusion_bc_multimodality_birdview_histogram.py
+screen -L -S action_analyzer_histogram .venv/bin/python eval_diffusion_bc_multimodality_birdview_histogram.py
