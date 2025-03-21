3a3c22abbd6ffb3320b8c1980cefbfdf34083d15
Gustavo Couto <gustavokarlcouto@gmail.com>
2025-03-20 22:52:58 -0300

    run 3000

---
+++ eval_diffusion_bc_multimodality_birdview_3000.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/
+export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+screen -L -S eval_diffusion_bc_multimodality_birdview_3000 .venv/bin/python eval_diffusion_bc_multimodality_birdview_3000.py
